c863fe5b5014adc0c3a915fe1de2ebaa802ca17b
A1t0r <1@1.ru>
2018-06-29 14:03:42 +0300

    Добавлена функция serpinsky

---
+++ etc/serpinsky.js
@@ -0,0 +1,18 @@
+function serpinsky(view, num, p1, p2, p3)
+{
+ if (num < 0)
+  return;
+ view.beginPath()
+ view.moveTo(p1[0], p1[1])
+ view.lineTo(p2[0], p2[1])
+ view.lineTo(p3[0], p3[1])
+ view.lineTo(p1[0], p1[1])
+ view.stroke()
+ var dp1 = [(p1[0]+p2[0])/2, (p1[1]+p2[1])/2]
+ var dp2 = [(p2[0]+p3[0])/2, (p2[1]+p3[1])/2]
+ var dp3 = [(p3[0]+p1[0])/2, (p3[1]+p1[1])/2]
+ iter(view, num-1, p1, dp1, dp3)
+ iter(view, num-1, dp1, p2, dp2)
+ iter(view, num-1, p3, dp3, dp2)
+}
+
